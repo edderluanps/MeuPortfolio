68b3b56757077b9d0d42305d7a143b91a8c70d23
Edder Luan <eluann100@gmail.com>
2022-03-29 21:26:41 -0300

    projetos

--- js/script.js
+++ js/script.js
@@ -12,4 +12,24 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            500:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 2,
+                nav: false
+            }
+        }
+    });
 });
